df4a728bb1e02e8708b01c5abcacbb16c90c72be
Sam Vergara <36249705+SamuelSVD@users.noreply.github.com>
2015-11-16 00:47:44 -0500

    Updated some classes. Starting to implement delay on visual components.

--- src/ProcessingJava/VisualComponent.java
+++ src/ProcessingJava/VisualComponent.java
@@ -1,14 +1,17 @@
 package ProcessingJava;
 import processing.core.*;
 abstract class VisualComponent extends PApplet{
-  Sketch sketch;
-  PVector position;
-  PVector colour;
-  float alpha;
+  protected Sketch sketch;
+  protected PVector position;
+  protected PVector colour;
+  protected float alpha;
+  protected boolean active;
+  protected float delay;
   VisualComponent(PVector position, PVector colour) {
     this.position = position;
     this.colour = colour;
     this.alpha = 255;
+    this.active = false;
   }
   public void setSketch(Sketch s) {
     this.sketch = s;
@@ -16,11 +19,20 @@ abstract class VisualComponent extends PApplet{
   public void setAlpha(float a) {
     this.alpha = a;
   }
+  public boolean isActive() { 
+    return active;
+  }
   public void draw(PVector shift) {
     PVector neg = new PVector(-shift.x, -shift.y);
     this.position.add(shift);
     this.draw();
     this.position.add(neg);
   }
-  public void update(float d) {};
+  public void setDelay(float d) {
+    this.delay = d;
+  }
+  public void update(float d) {
+    if (!active && delay > 0) delay -= d;
+    else if (!active) active = true;
+  }
 }
