8a9c419661a7e922009a02199993697e92fbe786
Sam Vergara <36249705+SamuelSVD@users.noreply.github.com>
2016-03-11 12:09:04 -0500

    Field3D_04.mp4 schedule

--- src/ProcessingJava/Field3D.java
+++ src/ProcessingJava/Field3D.java
@@ -41,7 +41,7 @@ public class Field3D extends VisualComponent{
         double z_2 = fun(x_2, y_2, t);
         double x_3 = x0 + dx*(i+1);
         double y_3 = y0 + dy*(j+1);
-        double z_3 = fun(x_3, y_3);
+        double z_3 = fun(x_3, y_3,t);
         row1.add((float)x_0);
         row1.add((float)y_0);
         row1.add((float)z_0);
@@ -68,7 +68,8 @@ public class Field3D extends VisualComponent{
   double fun(double x, double y, double t) {
     double r = Math.sqrt(x*x+y*y)/5;
     double a = 10;
-    return r!=0?a*Math.cos(r+2*PI/50*t)/r:2.5*a*Math.cos(2*PI/50*t);
+//    return r>0.9?a*Math.cos(r-2*PI/50*t)/r:a*Math.cos(r-2*PI/50*t);
+    return a*Math.cos(r-2*PI/50*t);
   }
 
 }
