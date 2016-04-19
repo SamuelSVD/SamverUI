44cf212c824390277af1dc02bbbd096b12977afd
Sam Vergara <36249705+SamuelSVD@users.noreply.github.com>
2016-04-18 22:13:12 -0400

    Updating code

--- src/ProcessingJava/MainDisplay.java
+++ src/ProcessingJava/MainDisplay.java
@@ -3,6 +3,7 @@ package ProcessingJava;
 import javax.swing.*;
 public class MainDisplay extends JFrame{
 	JPanel panel;
+	Sketch sketch;
 	static int MIN_SIZE = 150;
 	public MainDisplay(int x, int y) {
 	  if (x < MIN_SIZE) x = MIN_SIZE;
@@ -15,8 +16,19 @@ public class MainDisplay extends JFrame{
 		this.add(panel);
 	}
 	public void addSketch(Sketch s) {
-		panel.add(s);
-		s.init();
+	  if(sketch == null) {
+	    System.out.println('a');
+	    sketch = s;
+	    panel.add(s);
+	    s.init();
+	  }
+	  else {
+	    System.out.printf("b: %d", s.getVisualComponents().size());
+	    for(VisualComponent component: s.getVisualComponents()) {
+	      System.out.println('c');
+	      sketch.addVisualComponent(component);
+	    }
+	  }
 	}
 
 }
