c230d49960762890abc9fccf7bc97dc1f1b5e46f
SamuelSVD <SamuelSVD@localhost>
2016-07-17 11:17:16 -0400

    Added core libraries. Began SimpleVisuals.

---
+++ SimpleVisuals/Primitives/SVObject.java
@@ -0,0 +1,14 @@
+package Primitives;
+
+public class SVObject {
+	protected String name;
+	public SVObject(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ SimpleVisuals/Primitives/SVProperty.java
@@ -0,0 +1,17 @@
+package Primitives;
+
+public class SVProperty extends SVObject {
+	protected boolean isNumberic = false;
+	public boolean isNumberic() {
+		return isNumberic;
+	}
+	public void setNumberic(boolean isNumberic) {
+		this.isNumberic = isNumberic;
+	}
+	public SVProperty(String name) {
+		super(name);
+	}
+	public String strValue() {
+		return "";
+	}
+}
