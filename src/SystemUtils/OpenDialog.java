2e4e2ed848f6a43d63f5963f45e0cd1f2de65f57
Sam Vergara <36249705+SamuelSVD@users.noreply.github.com>
2016-11-14 21:11:04 -0500

    Added open dialog to FFT_Waves so you can choose input file.

--- src/SystemUtils/OpenDialog.java
+++ src/SystemUtils/OpenDialog.java
@@ -16,16 +16,26 @@ public class OpenDialog extends JFileChooser {
 	
 	public void SetFilter(String[] extensions, String[] descriptions) {
 		for (int i = 0; i < extensions.length; i++) {
-			this.addChoosableFileFilter(new FileFilter(){
-	      @Override
-	      public boolean accept(File f) {
-	      	if (f.getName().toLowerCase().endsWith(extensions[i])) {
-	      	  System.out.println("Returning true");
-	      	return true;
-	      	}
-	      	return false;
-	      }
-			});
+			this.addChoosableFileFilter(new Filter(extensions[i], descriptions[i]));
 		}
 	}
+
+	public class Filter extends FileFilter {
+		private String extension, description;
+		public Filter(String extension, String description) {
+			this.extension = extension;
+			this.description = description;
+		}
+		@Override
+		public boolean accept(File arg0) {			
+			return arg0.isDirectory() || arg0.getName().endsWith(extension);
+		}
+
+		@Override
+		public String getDescription() {
+			return description;
+		}
+		
+	}
 }
+
