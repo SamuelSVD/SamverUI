3866d5d487052d0c8d9824acab47587a1e32e100
Sam Vergara <36249705+SamuelSVD@users.noreply.github.com>
2017-01-29 12:14:36 -0500

    working on python shape maker

--- Data/Shapes/pygame_window.py
+++ Data/Shapes/pygame_window.py
@@ -4,7 +4,33 @@ import time
 from abc import ABCMeta, abstractmethod
 import math
 import datetime
+import easygui
+import os
+import json
 
+FILE_DIR = os.path.dirname(__file__)
+FILE_PATH = __file__
+print FILE_DIR
+print FILE_PATH
+psNone = 0
+psInsertBefore = 1
+psInsertAfter = 2
+psMove = 3
+program_states = ['NONE', 'INSERT BEFORE', 'INSERT AFTER', 'MOVE']
+class Options:
+	version = '1.0.0.0'
+	options = {'version': version}
+	options_path = FILE_DIR + '\config.json'
+	def __init__(self):
+		print 
+		self.Load()
+	def Load(self):
+		self.options = json.load(open(self.options_path,'r'))
+	def Save(self):
+		json.dump(self.options, open(self.options_path, 'w'))
+options = Options()
+options.Save()
+print FILE_PATH
 class Component:
 	__metaclass__= ABCMeta
 	positon = [0,0]
@@ -19,6 +45,16 @@ class Component:
 	def draw(self): pass
 	@abstractmethod
 	def handleEvent(self, event): pass
+	@abstractmethod
+	def OnMouseClick(self, event): pass
+	@abstractmethod
+	def OnMouseDrag(self, event): pass
+	@abstractmethod
+	def OnMouseRelease(self, event): pass
+	@abstractmethod
+	def OnKeyPress(self, event): pass
+	@abstractmethod
+	def OnKeyRelease(self, event): pass
 	
 class Point:
 	x = 0
@@ -96,24 +132,26 @@ class PointManager:
 			file.write(' ')
 		file.close()
 class ShapeMaker(Component):
-	NONE = 0
-	INSERT_BEFORE = 1
-	INSERT_AFTER = 2
-	MOVE = 3
-	states = ['NONE', 'INSERT BEFORE', 'INSERT AFTER', 'MOVE']
-	state = NONE
+	#ProgramStates
+	state = psNone
 	points = 0
 	font = 0
 	selectedPoint = 0
+	image = 0
+	offset = (0, 0)
 	def __init__(self, position):
 		pygame.font.init()
 		self.font = pygame.font.SysFont("Comic Sans MS", 16)
 	def setScreen(self, screen):
 		self.screen = screen
 		self.points = PointManager(screen, 100)
+		self.offset = (self.screen.get_width()/ 2, self.screen.get_height() / 2)
 	def update(self, delta): pass
 	def draw(self):
+		origin = self.offset
 		pygame.draw.rect(self.screen, (255,255,255), [0, 0, self.screen.get_width(), self.screen.get_height()	])
+		if self.image != 0:
+			self.screen.blit(self.image, origin)
 		for i in range(self.screen.get_width()/2 - 500, self.screen.get_width()/2 + 500, 100):
 			pygame.draw.line(self.screen, (150, 150, 150), (i, 0), (i, self.screen.get_height()))
 		pygame.draw.line(self.screen, (0, 0, 0), (self.screen.get_width()/2, 0), (self.screen.get_width()/2, self.screen.get_height()), 2)
@@ -121,59 +159,64 @@ class ShapeMaker(Component):
 			pygame.draw.line(self.screen, (150, 150, 150), (0, i), (self.screen.get_width(), i))
 		pygame.draw.line(self.screen, (0, 0, 0), (0, self.screen.get_height()/2), (self.screen.get_width(), self.screen.get_height()/2), 2)
 		
-		self.screen.blit(self.font.render(str(self.states[self.state]), False, (0,0,0)) ,(0,0))
-		self.screen.blit(self.font.render(str(self.selectedPoint), False, (0,0,0)) ,(0,20))
+		self.screen.blit(self.font.render(str(program_states[self.state]), False, (0,0,0)) ,(0,0))
+		self.screen.blit(self.font.render(options.version, False, (0,0,0)) ,(0,20))
 		self.points.draw()
 		self.points.drawSelected(self.selectedPoint)
-	def handleEvent(self, event):
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			mouse_pos = pygame.mouse.get_pos()
-			point = self.points.createPoint(mouse_pos)
-			if self.state == self.INSERT_AFTER:
-				self.points.insertAfter(self.selectedPoint, point)
-				self.selectedPoint = point
-			if self.state == self.INSERT_BEFORE:
-				self.points.insertBefore(self.selectedPoint, point)
-				self.selectedPoint = point
-			if self.state == self.NONE:
-				self.selectedPoint = self.points.select(mouse_pos, 0.1)
-			if self.state == self.MOVE:
-				if self.selectedPoint != 0:
-					self.selectedPoint.x = pygame.mouse.get_pos()[0]
-					self.selectedPoint.y = pygame.mouse.get_pos()[1]
-		if event.type == pygame.MOUSEBUTTONUP:
-			pass
-		if event.type == pygame.MOUSEMOTION:
-			mouse_pos = pygame.mouse.get_pos()
-			point = self.points.createPoint(mouse_pos)
-			if self.selectedPoint != 0 and event.buttons[0]:
-				self.selectedPoint.x = point.x;
-				self.selectedPoint.y = point.y;
-		if event.type == pygame.KEYUP:
-			pass
-		if event.type == pygame.KEYDOWN:
-			if event.key == K_c:
-				self.points.clear()
+	def handleEvent(self, event): pass
+	def OnMouseClick(self, event):
+		mouse_pos = pygame.mouse.get_pos()
+		point = self.points.createPoint(mouse_pos)
+		if self.state == psInsertAfter:
+			self.points.insertAfter(self.selectedPoint, point)
+			self.selectedPoint = point
+		if self.state == psInsertBefore:
+			self.points.insertBefore(self.selectedPoint, point)
+			self.selectedPoint = point
+		if self.state == psNone:
+			self.selectedPoint = self.points.select(mouse_pos, 0.1)
+		if self.state == psNone:
+			if self.selectedPoint != 0:
+				self.selectedPoint.x = pygame.mouse.get_pos()[0]
+				self.selectedPoint.y = pygame.mouse.get_pos()[1]
+	def OnMouseDrag(self, event):
+		mouse_pos = pygame.mouse.get_pos()
+		point = self.points.createPoint(mouse_pos)
+		if self.selectedPoint != 0 and event.buttons[0]:
+			self.selectedPoint.x = point.x;
+			self.selectedPoint.y = point.y;
+	def OnMouseRelease(self, event): pass
+	def OnKeyPress(self, event):
+		if event.key == K_c:
+			self.points.clear()
+			self.selectedPoint = 0
+		if event.key == K_i:
+			#try:
+				self.image = pygame.image.load(easygui.fileopenbox(filetypes = ['*.png', '*.jpg']))
+			#except:
+				pass
+		if event.key == K_SPACE:
+			filename = easygui.filesavebox(default = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')+'.pts', filetypes = ['*.pts'])
+			if filename: 
+				self.points.save( filename )
+		if event.key == K_INSERT:
+			if self.state == psInsertAfter:
+				self.state = psInsertBefore
+			elif self.state == psInsertBefore:
+				self.state = psNone
+			else:
+				self.state = psInsertAfter
+		if event.key == K_DELETE:
+			if self.selectedPoint != 0:
+				self.points.remove(self.selectedPoint)
 				self.selectedPoint = 0
-			if event.key == K_SPACE:
-				self.points.save( datetime.datetime.now().strftime('%Y%m%d_%H%M%S')+'.pts')
-			if event.key == K_INSERT:
-				if self.state == self.INSERT_AFTER:
-					self.state = self.INSERT_BEFORE
-				elif self.state == self.INSERT_BEFORE:
-					self.state = self.NONE
-				else:
-					self.state = self.INSERT_AFTER
-			if event.key == K_DELETE:
-				if self.selectedPoint != 0:
-					self.points.remove(self.selectedPoint)
-					self.selectedPoint = 0
-			if event.key == K_ESCAPE:
-				if self.state != self.NONE:
-					self.state = self.NONE
-				else:
-					CRASH;
-
+		if event.key == K_ESCAPE:
+			if self.state != psNone:
+				self.state = psNone
+			else:
+				CRASH;
+	def OnKeyRelease(self, event): pass
+	
 class Window:
 	def __init__(self, size, caption):
 		self.screen = pygame.display.set_mode(size)
@@ -193,7 +236,16 @@ class Window:
 					running = False
 					pygame.quit()
 				for c in self.components:
-					c.handleEvent(event)
+					if event.type == pygame.MOUSEBUTTONDOWN:
+						c.OnMouseClick(event)
+					elif event.type == pygame.MOUSEMOTION:
+						c.OnMouseDrag(event)
+					elif event.type == pygame.MOUSEBUTTONUP:
+						c.OnMouseRelease(event)
+					elif event.type == pygame.KEYDOWN:
+						c.OnKeyPress(event)
+					elif event.type == pygame.KEYUP:
+						c.OnKeyRelease(event)
 			if not running: break
 			delta = self.clock.tick()/1000.			
 			for c in self.components:
